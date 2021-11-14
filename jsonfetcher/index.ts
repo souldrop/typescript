ba6bd2dcd9f49963450f57b29868898096d25e5e
souldrop <imneocoder@gmail.com>
2021-11-14 09:50:55 +0530

    initialized learning typescript

---
+++ jsonfetcher/index.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+axios.get(url).then(response => {
+  const todo = response.data;
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+  console.log(`
+    The Todo with ID: ${id}
+    Has a title of: ${title}
+    Is it finished? ${completed}
+  `);
+});
